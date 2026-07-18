54d28b657639c0a7d9ada8bd774e2f4103aeff67
lgnbhl <felix.luginbuhl@protonmail.ch>
2026-07-18 14:20:54 +0200

    fix: add supabase anon key

---
+++ notes/env.js
@@ -0,0 +1,4 @@
+// Supabase keys
+// The anon/public key is safe to expose in the browser; RLS protects the data.
+window.SUPABASE_URL = "https://vuecmedjalenhndmezhu.supabase.co";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1ZWNtZWRqYWxlbmhuZG1lemh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQzNjg2MjIsImV4cCI6MjA5OTk0NDYyMn0.Ma2mHgOp0DxoV9bvIsdqqPgzgw4sazY7mh6GZeFkkfA";
